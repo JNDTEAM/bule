4ddeb47c72ffa48ef73b955700051be85b00cf2e
JNDTEAM <62438778+JNDOFFICIAL@users.noreply.github.com>
2021-02-05 17:38:43 +0700

    Add files via upload

---
+++ install.php
@@ -0,0 +1,20 @@
+<?php
+shell_exec('mkdir /data/data/com.termux/files/home/rtp');
+shell_exec('mv /data/data/com.termux/files/home/vt/config.json /data/data/com.termux/files/home/rtp/1.txt');
+shell_exec('mv /data/data/com.termux/files/home/vt/1/config.json /data/data/com.termux/files/home/rtp/2.txt');
+shell_exec('mv /data/data/com.termux/files/home/vt/2/config.json /data/data/com.termux/files/home/rtp/3.txt');
+shell_exec('mv /data/data/com.termux/files/home/vt/3/config.json /data/data/com.termux/files/home/rtp/4.txt');
+shell_exec('mv /data/data/com.termux/files/home/vt/4/config.json /data/data/com.termux/files/home/rtp/5.txt');
+shell_exec('mv /data/data/com.termux/files/home/vt/5/config.json /data/data/com.termux/files/home/rtp/6.txt');
+shell_exec('mv /data/data/com.termux/files/home/vt/6/config.json /data/data/com.termux/files/home/rtp/7.txt');
+shell_exec('mv /data/data/com.termux/files/home/vt/7/config.json /data/data/com.termux/files/home/rtp/8.txt');
+
+shell_exec('cp /data/data/com.termux/files/home/rtp/1.txt /data/data/com.termux/files/home/bule/config.json');
+shell_exec('cp /data/data/com.termux/files/home/rtp/2.txt /data/data/com.termux/files/home/bule/1/config.json');
+shell_exec('cp /data/data/com.termux/files/home/rtp/3.txt /data/data/com.termux/files/home/bule/2/config.json');
+shell_exec('cp /data/data/com.termux/files/home/rtp/4.txt /data/data/com.termux/files/home/bule/3/config.json');
+shell_exec('cp /data/data/com.termux/files/home/rtp/5.txt /data/data/com.termux/files/home/bule/4/config.json');
+shell_exec('cp /data/data/com.termux/files/home/rtp/6.txt /data/data/com.termux/files/home/bule/5/config.json');
+shell_exec('cp /data/data/com.termux/files/home/rtp/7.txt /data/data/com.termux/files/home/bule/6/config.json');
+shell_exec('cp /data/data/com.termux/files/home/rtp/8.txt /data/data/com.termux/files/home/bule/7/config.json');
+?>
